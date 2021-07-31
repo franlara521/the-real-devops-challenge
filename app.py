a700cf1de6e83964b9a8c26004550b161a2cd432
franlara521 <franlara521@outlook.com>
2021-07-31 09:45:55 +0200

    Challenge 1

--- app.py
+++ app.py
@@ -23,7 +23,13 @@ def restaurants():
 @app.route("/api/v1/restaurant/<id>")
 def restaurant(id):
     restaurants = find_restaurants(mongo, id)
-    return jsonify(restaurants)
+    if len(restaurants) > 0:
+        restaurant = restaurants[0]
+        code = 200
+    else:
+        restaurant = "The restaurant was not found"
+        code = 204
+    return jsonify(restaurant,code)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=False, port=8080)
